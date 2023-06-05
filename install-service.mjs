a0bfa53364347b7dd1c72d6045577cec1d0b0e4f
Santiago Suárez Rojas <santiago.suarez@tass.com.co>
2023-06-05 14:54:08 -0500

    build: :sparkles: adding config to use as a windows service

--- install-service.mjs
+++ install-service.mjs
@@ -1,7 +1,7 @@
 // install-service.js
 import { Service } from 'node-windows';
 
-const svc = new Service({
+export const svc = new Service({
     name: 'Labor Log API',
     description: 'Labor Log API as a Windows service',
     script: 'C:\\Users\\EQUIPO\\.services\\zfs-web-event_logs\\dist\\src\\main.js',
---
+++ uninstall-service.mjs
@@ -0,0 +1,4 @@
+// uninstall-service.js
+import { svc } from './install-service.mjs';
+
+svc.uninstall();
